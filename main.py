93720ad93b0e2ce8dbf6f49a337f71a5683681bf
NicolasM <nicolas.malgat@hotmail.fr>
2020-10-12 11:46:55 +0200

    commit avant de pull

--- main.py
+++ main.py
@@ -1,40 +1,32 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, exception_handlers
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
-http_exception = HTTPException(
-            400
-            , detail="""Les paramètres passés dans l'url ne sont pas complétés ou invalides:
-            metre_carre (int): le nombre de m² habitables loi Carrez. Defaults to None.
-            nb_pieces (int): nombre de pièces principales. Defaults to None.
-            terrain (int): le nombre de m² du terrain. Defaults to None.
-            code_postal (int): le code postal où se trouve le bien. Defaults to None.
-            Exemple: https://website.com/maison?metre_carre=200&nb_piece=5&terrain=100&code_postal=63800"""
-            , headers={"Paramètres invalides"}
-        )
+@app.exception_handler(StarletteHTTPException)
+async def custom_http_exception_handler(request, exc):
+    print(f"OMG! An HTTP error!: {repr(exc)}")
+    return await http_exception_handler(request, exc)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    print(f"OMG! The client sent invalid data!: {exc}")
+    return await request_validation_exception_handler(request, exc)
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return JSONResponse(content=jsonable_encoder({"message": "Hello world !"}))
 
-    
 @app.get("/maison")
-async def estimation_maison(metre_carre: int = None, nb_pieces: int = None, terrain: int = None, code_postal: int = None):
-    """Renvoie une estimation pour la maison passée en paramètre
-
-    Args:
-        metre_carre (int): le nombre de m² habitables loi Carrez. Defaults to None.
-        nb_pieces (int): nombre de pièces principales. Defaults to None.
-        terrain (int): le nombre de m² du terrain. Defaults to None.
-        code_postal (int): le code postal où se trouve le bien. Defaults to None.
-
-    Returns:
-        json(jsp): Une estimation de la valeur de la maison
-    """
-    error = ""
-    if not(metre_carre and nb_pieces and terrain and code_postal):
-        raise http_exception
-    
-    raise NotImplemented()
+async def estimation_maison(metre_carre: int, nb_pieces: int, terrain: int, code_postal: int):    
+    raise HTTPException(404, detail="This api is not implemented yet :(")
     # return intelligence_artificielle.estimation_maison(metre_carre, nb_pieces, terrain, code_postal)
     
